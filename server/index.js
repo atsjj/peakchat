004491d179073e7b40d6a44977fc1e4723a016e9
Steve Jabour <steve@jabour.me>
2015-06-10 10:52:11 -0600

    Switched to older server code.

--- server/index.js
+++ server/index.js
@@ -1,21 +1,22 @@
-var WebSocketServer = require('ws').Server;
+// var WebSocketServer = require('ws').Server;
+var WebSocketServer = require('websocket').server;
 
 module.exports = function(app, options) {
-  var sockets = new WebSocketServer({
-    server: options.httpServer,
-    path: '/api'
-  });
+  // var sockets = new WebSocketServer({
+  //   server: options.httpServer,
+  //   path: '/api'
+  // });
 
-  sockets.on('connection', function(ws) {
-    var id = setInterval(function() {
-    ws.send(JSON.stringify(process.memoryUsage()), function() { /* ignore errors */ });
-    }, 100);
-    console.log('started client interval');
-    ws.on('close', function() {
-      console.log('stopping client interval');
-      clearInterval(id);
-    });
-  });
+  // sockets.on('connection', function(ws) {
+  //   var id = setInterval(function() {
+  //   ws.send(JSON.stringify(process.memoryUsage()), function() { /* ignore errors */ });
+  //   }, 100);
+  //   console.log('started client interval');
+  //   ws.on('close', function() {
+  //     console.log('stopping client interval');
+  //     clearInterval(id);
+  //   });
+  // });
 
   // sockets.on('connection', function(socket) {
   //   socket.on('open', function() {});
@@ -24,96 +25,96 @@ module.exports = function(app, options) {
   //   socket.on('message', function() {});
   // });
 
-  // var sockets = new WebSocketServer({
-  //   autoAcceptConnections: false,
-  //   httpServer: options.httpServer,
-  //   path: '/api'
-  // });
+  var sockets = new WebSocketServer({
+    autoAcceptConnections: false,
+    httpServer: options.httpServer,
+    path: '/api'
+  });
 
-  // sockets.on('request', onRequest);
+  sockets.on('request', onRequest);
 };
 
 // shared stuff
 
-// var CHANNELS = { };
+var CHANNELS = { };
 
-// function onRequest(socket) {
-//     var origin = socket.origin + socket.resource;
+function onRequest(socket) {
+    var origin = socket.origin + socket.resource;
 
-//     var websocket = socket.accept(null, origin);
+    var websocket = socket.accept(null, origin);
 
-//     websocket.on('message', function(message) {
-//         if (message.type === 'utf8') {
-//             onMessage(JSON.parse(message.utf8Data), websocket);
-//         }
-//     });
+    websocket.on('message', function(message) {
+        if (message.type === 'utf8') {
+            onMessage(JSON.parse(message.utf8Data), websocket);
+        }
+    });
 
-//     websocket.on('close', function() {
-//         truncateChannels(websocket);
-//     });
-// }
+    websocket.on('close', function() {
+        truncateChannels(websocket);
+    });
+}
 
-// function onMessage(message, websocket) {
-//     if (message.checkPresence)
-//         checkPresence(message, websocket);
-//     else if (message.open)
-//         onOpen(message, websocket);
-//     else
-//         sendMessage(message, websocket);
-// }
+function onMessage(message, websocket) {
+    if (message.checkPresence)
+        checkPresence(message, websocket);
+    else if (message.open)
+        onOpen(message, websocket);
+    else
+        sendMessage(message, websocket);
+}
 
-// function onOpen(message, websocket) {
-//     var channel = CHANNELS[message.channel];
+function onOpen(message, websocket) {
+    var channel = CHANNELS[message.channel];
 
-//     if (channel)
-//         CHANNELS[message.channel][channel.length] = websocket;
-//     else
-//         CHANNELS[message.channel] = [websocket];
-// }
+    if (channel)
+        CHANNELS[message.channel][channel.length] = websocket;
+    else
+        CHANNELS[message.channel] = [websocket];
+}
 
-// function sendMessage(message, websocket) {
-//     message.data = JSON.stringify(message.data);
-//     var channel = CHANNELS[message.channel];
-//     if (!channel) {
-//         console.error('no such channel exists');
-//         return;
-//     }
+function sendMessage(message, websocket) {
+    message.data = JSON.stringify(message.data);
+    var channel = CHANNELS[message.channel];
+    if (!channel) {
+        console.error('no such channel exists');
+        return;
+    }
 
-//     for (var i = 0; i < channel.length; i++) {
-//         if (channel[i] && channel[i] != websocket) {
-//             try {
-//                 channel[i].sendUTF(message.data);
-//             } catch(e) {
-//             }
-//         }
-//     }
-// }
+    for (var i = 0; i < channel.length; i++) {
+        if (channel[i] && channel[i] != websocket) {
+            try {
+                channel[i].sendUTF(message.data);
+            } catch(e) {
+            }
+        }
+    }
+}
 
-// function checkPresence(message, websocket) {
-//     websocket.sendUTF(JSON.stringify({
-//         isChannelPresent: !!CHANNELS[message.channel]
-//     }));
-// }
+function checkPresence(message, websocket) {
+    websocket.sendUTF(JSON.stringify({
+        isChannelPresent: !!CHANNELS[message.channel]
+    }));
+}
 
-// function swapArray(arr) {
-//     var swapped = [],
-//         length = arr.length;
-//     for (var i = 0; i < length; i++) {
-//         if (arr[i])
-//             swapped[swapped.length] = arr[i];
-//     }
-//     return swapped;
-// }
+function swapArray(arr) {
+    var swapped = [],
+        length = arr.length;
+    for (var i = 0; i < length; i++) {
+        if (arr[i])
+            swapped[swapped.length] = arr[i];
+    }
+    return swapped;
+}
 
-// function truncateChannels(websocket) {
-//     for (var channel in CHANNELS) {
-//         var _channel = CHANNELS[channel];
-//         for (var i = 0; i < _channel.length; i++) {
-//             if (_channel[i] == websocket)
-//                 delete _channel[i];
-//         }
-//         CHANNELS[channel] = swapArray(_channel);
-//         if (CHANNELS && CHANNELS[channel] && !CHANNELS[channel].length)
-//             delete CHANNELS[channel];
-//     }
-// }
+function truncateChannels(websocket) {
+    for (var channel in CHANNELS) {
+        var _channel = CHANNELS[channel];
+        for (var i = 0; i < _channel.length; i++) {
+            if (_channel[i] == websocket)
+                delete _channel[i];
+        }
+        CHANNELS[channel] = swapArray(_channel);
+        if (CHANNELS && CHANNELS[channel] && !CHANNELS[channel].length)
+            delete CHANNELS[channel];
+    }
+}
