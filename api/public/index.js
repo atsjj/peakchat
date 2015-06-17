04763ade83d16a33616715be436e65582dcfd18e
Steve Jabour <steve@jabour.me>
2015-06-17 16:08:09 -0600

    Trying fastboot again.

--- api/public/index.js
+++ api/public/index.js
@@ -1,9 +1,31 @@
 /* global require, module */
 'use strict';
 
+var path            = require('path');
+
+var moduleBase      = path.join(process.cwd(), 'api', 'node_modules');
+var serverBase      = path.join(process.cwd(), 'server');
+var debugBase       = path.join(moduleBase, 'ember-cli', 'node_modules', 'debug');
+
+var appFile         = path.join(serverBase, 'assets', 'api.js');
+var vendorFile      = path.join(serverBase, 'assets', 'vendor.js');
+var emberAppFile    = path.join(moduleBase, 'ember-cli-fastboot', 'lib', 'models', 'ember-app.js');
+
+var debug           = require(debugBase)('peakchat-api:middleware');
+
 var WebSocketServer = require('ws')['Server'];
+var EmberApp        = require(emberAppFile);
 
 module.exports = function api(app, options) {
+  try {
+    var emberApp = new EmberApp({
+      appFile: appFile,
+      vendorFile: vendorFile
+    });
+  } catch(e) {
+    debug('emberApp error %o', e);
+  }
+
   var sockets = new WebSocketServer({
     server: options.httpServer,
     path: '/api'
